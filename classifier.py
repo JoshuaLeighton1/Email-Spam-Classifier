6259fa725f7d2125d29665d4c96a201c471c882a
JoshuaLeighton1 <joshuaroelf@gmail.com>
2025-05-24 18:29:44 +0200

    add preprocessing logic and imports

--- classifier.py
+++ classifier.py
@@ -1,8 +1,16 @@
+# %%
 import pandas as pd
 import numpy as np
+import re
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
+
+#Run once
+nltk.download('stopwords')
+
 
 #Load dataset
-
 df = pd.read_csv('combined_data.csv', encoding="latin-1", header=None)
 
 df.columns=['label', 'text']
@@ -10,3 +18,18 @@ df = df[['label', 'text']]
 
 df = df.sample(n=5000, random_state=42)
 df.describe(include="all")
+
+
+def preprocess(text):
+    #Remove all special characters and noise keep only letters and space
+    text = re.sub(r'[^A-Za-z\s]', '', text)
+    #convert all text to lowercase to ensure consistency
+    text = text.lower()
+    words = text.split()
+    #remove all stopwords: those that have little or no sentiment 
+    stop_words = set(stopwords.words('english'))
+    words = [words for word in words if word not in stop_words]
+    #apply stemming
+    stemmer = PorterStemmer()
+    words = [stemmer.stem(word) for word in words]
+    return ' '.join(words)

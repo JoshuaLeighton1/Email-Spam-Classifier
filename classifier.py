d921416ec18ad442ff0a7b85a888bd7d0d8f1251
JoshuaLeighton1 <joshuaroelf@gmail.com>
2025-05-24 19:38:56 +0200

    Add Model training and evaluation

--- classifier.py
+++ classifier.py
@@ -5,8 +5,10 @@ import re
 import nltk
 from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
+from sklearn.naive_bayes import MultinomialNB
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 
 #Run once
 nltk.download('stopwords')
@@ -52,10 +54,29 @@ y = df['label']
 #Split data into training and testing sets
 
 X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.2, random_state=42)
+#Train the model 
+model = MultinomialNB(max_iter=1000)
+model.fit(X_train,y_train)
+
+#Evaluate the model 
+y_pred = model.predict(X_test)
+#Proportion of correct predictions
+accuracy = accuracy_score(y_test, y_pred)
+#accuracy of positive predictions 
+precision = precision_score(y_test, y_pred)
+#Ability for detection 
+recall = recall_score(y_test, y_pred)
+f1 = f1_score(y_test, y_pred)
 
 
+print(f"Accuracy: {accuracy:.4f}")
+print(f"Precision: {precision:.4f}")
+print(f"Recall: {recall:.4f}")
+print(f"F1-score: {f1:.4f}")
 
 
 
 #test 
 #print(preprocess_text("Hello@ I am sending yoU a Ema@il"))
+
+## %%
